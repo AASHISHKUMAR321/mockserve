73aa2ce6e43fe4b43bf1ebd916259503eddb05a1
AASHISHKUMAR321 <you@example.com>
2023-01-19 13:32:39 +0530

    server

--- config.js
+++ config.js
@@ -7,13 +7,8 @@ export default {
     { route: "/comments", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/photos", methods: ["POST", "PUT", "DELETE", "PATCH"] },
     { route: "/todos", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/recipeCategories", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/recipeIngredients", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/recipes", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/areas", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/recipeTags", methods: ["POST", "PUT", "DELETE", "PATCH"] },
-    { route: "/orders", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
-  ]
-}
+    { route: "/products", methods: ["GET", "POST", "PUT", "DELETE", "PATCH"] },
+  ],
+};
 
 // Employees, departments & cats are not protected.
